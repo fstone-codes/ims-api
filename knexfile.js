e70de9d62b53e924baeeeb33c3be1219f2fbe75e
fstone <164958630+fstone-codes@users.noreply.github.com>
2025-03-01 13:23:44 -0500

    edit connection env variables for deployment

--- knexfile.js
+++ knexfile.js
@@ -4,17 +4,12 @@ import "dotenv/config";
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-
-const connection = process.env.DB_URL
-    ? process.env.DB_URL
-    : {
-          host: process.env.DB_HOST,
-          database: process.env.DB_LOCAL_DBNAME,
-          user: process.env.DB_LOCAL_USER,
-          password: process.env.DB_LOCAL_PASSWORD,
-      };
-
 export default {
     client: "mysql2",
-    connection,
+    connection: {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
 };
